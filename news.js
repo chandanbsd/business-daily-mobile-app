6ded74bcc5c810314ef5dfc6cec803b689dd6a8c
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2023-02-01 17:57:07 -0500

    Dependency updates, port from expo sdk 43.0 to 47.0

--- news.js
+++ news.js
@@ -1,10 +1,17 @@
-import { NEWS_API_KEY } from '@env'
+import { NEWS_API_KEY } from "@env";
 
-const url = "https://newsapi.org/v2/everything?q=microsoft&apiKey=" + NEWS_API_KEY;
+const url =
+  "https://newsapi.org/v2/everything?q=microsoft&apiKey=" +
+  "***REMOVED***";
+console.log(url);
 
 export async function getNews(companyName) {
-  console.log(companyName)
-  let result = await fetch("https://newsapi.org/v2/everything?q="+companyName +"&apiKey=" + NEWS_API_KEY)
-  .then(response => response.json());
+  console.log(companyName);
+  let result = await fetch(
+    "https://newsapi.org/v2/everything?q=" +
+      companyName +
+      "&apiKey=" +
+      "***REMOVED***"
+  ).then((response) => response.json());
   return result.articles;
 }
